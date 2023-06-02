ebbd963ae6be201e0fad1015ef9dec6c7d6aa76e
consolexyz <mosesobike@gmail.com>
2023-06-02 01:27:54 +0100

    fixed buy button

--- my-rainbowkit-app/components/Main.jsx
+++ my-rainbowkit-app/components/Main.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 
 function Main () {
+
+    function buy () {
+        console.log('bought');
+    }
     return ( 
         <div className='main'>
             <h1 className='main-text'>Buy Xyz a Coffee</h1>
             <form>
-                <label>Send a message</label><br></br>
+                <label>Send a message</label>
                 <textarea type="text"
                   placeholder="Enjoy your coffee!"
                   id="message"
                   required></textarea>
-            <button type='button' className='form-button'>
-                Send 0.001ETH
+            <button type='button' className='form-button' onClick={buy}>
+                Buy
             </button>
         </form>
         </div>
